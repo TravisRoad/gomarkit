bfe849d53d2fb96961630a247f5249592f076564
Travis <lxymontage@gmail.com>
2023-12-02 17:49:36 +0800

    change router err handler

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func main() {
 
 	r := setup.InitRouter()
 	if err := r.Run(fmt.Sprintf("0.0.0.0:%d", global.Config.Port)); err != nil {
-		slog.Error("failed to run server on 0.0.0.0:%d: %v", global.Config.Port, err)
+		slog.Error("failed to run server on 0.0.0.0", slog.Int("port", global.Config.Port), err)
 	}
 }
