69617a593583a8716943e8ea3dcdeb93b2febb82
Travis <lxymontage@gmail.com>
2023-12-07 18:24:12 +0800

    添加日志轮转

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	setup.Setup()
 
+	global.Logger.Info("Setup Success")
+
 	r := setup.InitRouter()
 	if err := r.Run(fmt.Sprintf("0.0.0.0:%d", global.Config.Port)); err != nil {
 		slog.Error("failed to run server on 0.0.0.0", slog.Int("port", global.Config.Port), err)
--- setup/log.go
+++ setup/log.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func PathExists(path string) (bool, error) {
@@ -18,9 +20,23 @@ func PathExists(path string) (bool, error) {
 }
 
 func initZap() *zap.Logger {
-	logger, err := zap.NewDevelopment(zap.AddCaller())
-	if err != nil {
-		panic(err)
-	}
+	// logger, err := zap.NewDevelopment(zap.AddCaller())
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "./logs/foo.log",
+		MaxSize:    1, // megabytes
+		MaxBackups: 30,
+		MaxAge:     28, // days
+	})
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()),
+		w,
+		zap.DebugLevel,
+	)
+	logger := zap.New(core, zap.AddCaller())
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 	return logger
 }
